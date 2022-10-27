d125cc3a265f69eb5f7c34f23db69cf9d8ad07d2
leahmarv <lmarville@smith.edu>
2022-10-27 12:05:01 -0400

    Called Building super class methods for House

--- House.java
+++ House.java
@@ -1,12 +1,21 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/* This is a stub for the House class */
+public class House extends Building {
+
+
+  ArrayList<String> residents;
+  boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super (name, address, nFloors);
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = new ArrayList<>();
     System.out.println("You have built a house: 🏠");
   }
 
   public static void main(String[] args) {
-    new House();
+    //new House();
   }
 
 }

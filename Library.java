6fd4cbc0b0ccc2b3d9e1bfe4ad366f27ab57d015
leahmarv <lmarville@smith.edu>
2022-11-01 21:42:24 -0400

    Outlined methods for Library

--- Library.java
+++ Library.java
@@ -1,8 +1,29 @@
+import java.util.Hashtable;
+
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building {
+
+  private Hashtable <String, Boolean> collection;
 
     public Library() {
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
+    }
+
+    public void addTitle(String title) {
+
+    }
+
+    public String removeTitle(String title) {
+
+    }
+
+    public void checkOut(String title) {
+
+    }
+
+    public void return(String title) {
+  
     }
   
     public static void main(String[] args) {
